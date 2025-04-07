5cb3d5a16e1f14cd690ceda4bd1ff60c8cfcbdf4
azbin-spec <dydouhabdellah82@gmail.com>
2025-04-07 18:10:51 +0200

    changed the 2 because of errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,24 +1,30 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_node - Create a binary node
- * @parent: node parent
- * @value: Value of the node
- * Return: the new node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Return: a pointer to the created node or NULL if fails
  */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
+	if (!parent)
 		return (NULL);
+
+	new_node = binary_tree_node(NULL, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right)
+	{
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
 	}
-	new_node->n = value;
+
+	parent->right = new_node;
 	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 	return (new_node);
 }
