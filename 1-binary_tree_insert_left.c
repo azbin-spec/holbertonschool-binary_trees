476c8b63658b5987d620fb78a2c5d88a0b3915cd
LouisManchon <10578@holbertonstudents.com>
2025-04-07 20:09:39 +0200

    Update 1-binary_tree_insert_left.c and add 1-main.c 3-main.c and binary_tree_print.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (new_node == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
 	new_node->n = value;
 	new_node->parent = parent;
