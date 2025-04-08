f15d96f291fe414b8b5024144e2ed3b5aefd18f5
azbin-spec <dydouhabdellah82@gmail.com>
2025-04-08 08:12:37 +0200

    added 2 task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,30 +1,31 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
- * @parent: pointer to the node to insert the right-child in
- * @value: value to store in the new node
- *
- * Return: a pointer to the created node or NULL if fails
+ * *binary_tree_insert_right - inserts a node as the right-child
+ * of another node
+ * Return: binary tree node
+ * @parent: parent of new node
+ * @value: value of new node
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
 
-	binary_tree_t *new_node;
-
-	if (!parent)
-		return (NULL);
-
-	new_node = binary_tree_node(NULL, value);
-	if (!new_node)
-		return (NULL);
-
-	if (parent->right)
+	if (parent == NULL)
 	{
-		parent->right->parent = new_node;
-		new_node->right = parent->right;
+		return (NULL);
 	}
 
-	parent->right = new_node;
-	new_node->parent = parent;
-	return (new_node);
+	new = binary_tree_node(parent, value);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		new->right->parent = new;
+	}
+
+	parent->right = new;
+
+	return (new);
 }
