0b3efac4c91785167aa557a6626cf24e7595bfbc
azbin-spec <dydouhabdellah82@gmail.com>
2025-04-10 11:04:48 +0200

    fixed errors

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,17 +1,21 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_uncle - finds the uncle of a node
- * @node: a pointer to the node to find the uncle
- *
- * Return: binary_tree_t - a pointer to the uncle node or NULL if fails
+ * *binary_tree_uncle - Finds the uncle of a node.
+ * @node: The pointer to the node to find the sibling.
+ * Return: If the pointer to the uncle node or (NULL).
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
+	{
 		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
 
-	if (node->parent->parent->right == node->parent)
-		return (node->parent->parent->left);
-
-	return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
